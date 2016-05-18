73591e1b3cf32333bdfd4e70c117c6b5b46e93c7
Adrian R. Bazaga <alu0100826456@ull.edu.es>
2016-05-18 20:51:29 +0100

    Fixed some things from mainfromfile.js

--- mainfromfile.js
+++ mainfromfile.js
@@ -1,15 +1,19 @@
+/* Chuchu++: A multiparadigm meta-language built using PEGjs as parser for the
+ * Language Processors subject at Universidad de La Laguna, Grado en Ingeniería Informática.
+ * 
+ * Team: Adrián Rodríguez Bazaga (arodriba@ull.edu.es) & Rudolf Cicko * 
+ */
+
 #!/usr/local/bin/node --harmony_destructuring
 "use strict";
 const process = require('process');
-/* See gist: https://gist.github.com/branneman/8048520 
- * Better local require() paths for Node.js */
 process.env.NODE_PATH += ":"+__dirname+"/lib/";
 require('module').Module._initPaths();
 var util = require('util');
 var fs = require('fs');
 var PEG = require("pl0node.js");
-var semantic = require('./lib/semantic.js');					// Require the semantic module
-let semanticFunc = semantic.semantic;								// Import the semantic function
+var semantic = require('./lib/semantic.js');						// Require the semantic module
+let semanticPhase = semantic.semantic;								// Import the semantic function
 var fileName = process.argv[2] || 'tests/input5.pl0';
 
 const lineNumbers = (input) => {
@@ -27,9 +31,10 @@ fs.readFile(fileName, 'utf8', function (err,input) {
   console.log(`Processing <***\n${lineNumbers(input)}\n***>`);
   try {
     var r = PEG.parse(input);	// Syntactic phase
-    semanticFunc(r);		// Semantic phase
+    semanticPhase(r);			// Semantic phase
     console.log(util.inspect(r, {depth: null}));	// Show tree
   } catch (e) {
+	// Catch the error and show the information about it (Usually from the semantic phase)
     console.log(`Error at line ${e.startLine}, column ${e.startColumn}. Message: ${e.message}`);
   }
 });
